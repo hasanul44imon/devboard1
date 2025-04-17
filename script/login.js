5ec8385536fc4773c1eebc0d8bb3ef05638ba04b
imon28 <hasanu44imon@gmail.com>
2025-04-18 01:56:17 +0600

    added some

--- script/login.js
+++ script/login.js
@@ -1,7 +1,9 @@
 // Button 
-const completeBTn = document.querySelectorAll('.complete-btn');
-const taskCount = document.getElementById('left-count')
+const completeBtn = document.querySelectorAll('.complete-btn');
+// console.log(completeBtn.length)
+const taskCount = document.getElementById('left-count');
 taskCount.innerText = completeBtn.length.toString().padStart(2, '0');
+
 
 for (let i = 0; i < completeBtn.length; i++) {
     const element = completeBtn[i];
@@ -16,7 +18,7 @@ for (let i = 0; i < completeBtn.length; i++) {
         taskCount.innerText = count.toString().padStart(2, '0');
 
         // Total Count
-        const countTask = document.getElementById('count-task');
+        const countTask = document.getElementById('task-count');
         let countNumber = parseInt(countTask.innerText);
         countNumber = countNumber + 1;
         countTask.innerText = countNumber.toString().padStart(2, '0');
@@ -26,3 +28,17 @@ for (let i = 0; i < completeBtn.length; i++) {
         if (count == 0) {
             alert("Congrats!!! You have complete all the current task.");
         }
+
+    })}
+
+   // Current Date & Time
+let currentDateElement = document.getElementById('current-date');
+let currentDate = new Date();
+let options = { year: 'numeric', month: 'short', day: 'numeric' };
+let date = currentDate.toLocaleDateString('en-US', options);
+currentDateElement.innerText = date;
+
+let currentDay = document.getElementById('current-day');
+let optionsDay = { weekday: 'short' };
+let day = currentDate.toLocaleDateString("en-us", optionsDay);
+currentDay.innerText = day + ",";
